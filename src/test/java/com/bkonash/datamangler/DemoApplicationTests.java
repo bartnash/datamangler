c2767e2372212353ea25e0fa2bba521fb0813b76
Bart Nash <bart@bkonash.com>
2023-12-04 06:18:07 -0600

    Initial buildout of application.

---
+++ src/test/java/com/bkonash/datamangler/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkonash.datamangler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
